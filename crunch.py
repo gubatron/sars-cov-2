6a8c172f59e756341df5a1940bd10ed107daaa4e
gubatron <gubatron@gmail.com>
2020-04-04 04:30:27 -0600

    New Daily Dead column. K adjusted to a period of 3 days only

--- crunch.py
+++ crunch.py
@@ -21,15 +21,16 @@ COUNTRY=1
 DEAD=4
 
 class record:
-  def __init__(self, date, month, day, year, dead, k):
+  def __init__(self, date, month, day, year, dead, k, daily_dead):
     self.date = date
     self.month = month
     self.day = day
     self.year = year
     self.dead = dead
     self.k = k
+    self.daily_dead = daily_dead
   def __str__(self):
-    return "{0},{1},{2}".format(self.date,self.dead,self.k)
+    return "{0},{1},{2},{3}".format(self.date, self.dead, self.k, self.daily_dead)
 
 def zero_pad(n):
   if n < 10:
@@ -59,7 +60,7 @@ def monthly_numbers(results, month, country):
   end_day = calendar.monthrange(2020, month)[1] + 1
   if month == 1:
     start_day=22
-  prev_dead = 0
+  prev_k_dead = 0
   for day in range(start_day, end_day):
     if month >= 3 and day >= 22 and COUNTRY==1:
       # new fields were added on 03-23-2020
@@ -69,16 +70,20 @@ def monthly_numbers(results, month, country):
     if dead == None:
       continue
     k=0
-    k_period=7
+    k_period=3
     if len(results) > k_period:
       last_record = results[-(k_period+1)]
-      prev_dead = last_record.dead
+      prev_k_dead = last_record.dead
 
-    if prev_dead > 0:
-      k = math.log(dead/prev_dead)/k_period
-      print("day={0} results[-8] -> prev_dead={1} dead={2} k={3} log({4}/{5})/7".format(day, prev_dead, dead, k, prev_dead, dead))
+    daily_dead = dead
+    if len(results) > 0:
+      daily_dead = dead - results[-1].dead
+    
+    if prev_k_dead > 0:
+      k = math.log(dead/prev_k_dead)/k_period
+      print("day={0} results[-8] -> prev_k_dead={1} dead={2} k={3} log({4}/{5})/3 daily_dead={6}".format(day, prev_k_dead, dead, k, prev_k_dead, dead, daily_dead))
 
-    r = record('{0}-{1}-2020'.format(zero_pad(month), zero_pad(day)), month, day, 2020, dead, k) 
+    r = record('{0}-{1}-2020'.format(zero_pad(month), zero_pad(day)), month, day, 2020, dead, k, daily_dead)
     results.append(r)
 
 def crunch_country_report(country):
@@ -88,7 +93,7 @@ def crunch_country_report(country):
     monthly_numbers(results, month, country)
   filename = 'output_' + country + '.csv'
   with open(filename,'w') as output:
-    output.write('Date,Dead,K\n')
+    output.write('Date,Total Dead,K,Daily Dead\n')
     for r in results:
       output.write(str(r))
       output.write('\n')
